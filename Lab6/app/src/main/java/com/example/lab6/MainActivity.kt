d2f1197299a6bacf08e4e316fbc9e8e7d0de9e92
Tushit Nag <tushitnag@gmail.com>
2020-10-25 19:28:32 -0700

    Team Page Display Done

--- Lab6/app/src/main/java/com/example/lab6/MainActivity.kt
+++ Lab6/app/src/main/java/com/example/lab6/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+import java.io.IOException
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -14,15 +16,21 @@ class MainActivity : AppCompatActivity() {
 
         val teamList = ArrayList<Team>()
 
+        private val client = OkHttpClient()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        AddData()
+        //AddData()
+        run("http://54.145.163.174:5000/team")
 
         recyclerView.adapter = TeamAdapter(teamList)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.setHasFixedSize(true)
+
+
+
     }
 
 
@@ -35,6 +43,15 @@ class MainActivity : AppCompatActivity() {
             teamList.add(item)
         }
     }
-
+    fun run(url: String) {
+        val request = Request.Builder()
+            .url(url)
+            .build()
+println("============= call api")
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {}
+            override fun onResponse(call: Call, response: Response) = println(response.body()?.string())
+        })
+    }
 
 }
