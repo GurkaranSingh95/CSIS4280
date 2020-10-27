ae4d65a324904cd53bfc9f2fb3b1d29abe51262e
Tushit Nag <tushitnag@gmail.com>
2020-10-26 17:10:01 -0700

    Changes to Tablayout

--- Lab6/app/src/main/java/com/example/lab6/MainActivity.kt
+++ Lab6/app/src/main/java/com/example/lab6/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.lab6
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.OkHttpClient
 import java.io.IOException
@@ -14,25 +16,67 @@ import kotlin.collections.ArrayList
 class MainActivity : AppCompatActivity() {
     private val teamListLiveData = MutableLiveData<List<Team>>()
 
-    val teamList = ArrayList<Team>()
-
-    private val client = OkHttpClient()
-
+    var teamService= TeamService()
     var dataService: DataService = DataService()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    var context = this
 
-        dataService.run("http://54.225.179.78:5000/data", this)
+
+
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                when(tabLayout.selectedTabPosition)
+                {
+                    0 -> {
+
+
+//                        teamService.run("http://54.225.179.78:5000/team",context)
+
+                    }
+                    1 -> {
+
+                        dataService.run("http://54.225.179.78:5000/data", context)
+                    }
+
+
+                }
+
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+
+        })
+
+
+       teamService.teamListLiveData.observe(this, Observer { ms ->
+            ms?.let {
+                println("NEW LIST: " + it.size)
+                recyclerView.adapter = TeamAdapter(it)
+            }
+
+        })
 
         dataService.dataListLiveData.observe(this, Observer { ms ->
             ms?.let {
                 println("NEW LIST: " + it.size)
+
+                recyclerView.adapter = DataAdapter(it)
             }
+
         })
 
-        recyclerView.adapter = TeamAdapter(emptyList())
+
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.setHasFixedSize(true)
 
